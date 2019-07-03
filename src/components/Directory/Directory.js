46ed7d4a4c781bb3606cae9827f6271874eeac75
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-07-03 15:21:48 +0300

    add MenuItem withRouter history push

--- src/components/Directory/Directory.js
+++ src/components/Directory/Directory.js
@@ -6,8 +6,8 @@ import MenuItem from '../MenuItem/MenuItem'
 const Directory = () => {
   return (
     <div className="directory-menu">
-      {sections.map(({ id, title, imageUrl, linkUrl, size }) => (
-        <MenuItem title={title} key={id} imageUrl={imageUrl} linkUrl={linkUrl} size={size} />
+      {sections.map(({ id, ...otherSectionProps }) => (
+        <MenuItem {...otherSectionProps} key={id} />
       ))}
     </div>
   )
