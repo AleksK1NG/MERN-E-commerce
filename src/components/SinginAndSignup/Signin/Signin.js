ba855e82332ab4105641009695809cda1c04e176
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-07-03 16:57:48 +0300

    add Signin initial form

--- src/components/SinginAndSignup/Signin/Signin.js
+++ src/components/SinginAndSignup/Signin/Signin.js
@@ -3,10 +3,27 @@ import './Signin.styles.scss'
 import { useForm } from '../../../hooks/useForm'
 
 const Signin = () => {
-  const [values, handleChange] = useForm({ email: "", password: "" })
+  const [values, handleChange] = useForm({ email: '', password: '' })
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    console.log('Submit form => ', values)
+  }
+
   return (
-    <div>
-      Signin
+    <div className="sing-in">
+      <h2>I already have an account</h2>
+      <span>Sign in with your email and password</span>
+
+      <form onSubmit={handleSubmit}>
+        <input type="email" name="email" value={values.email} onChange={handleChange} required />
+        <label htmlFor="">Email</label>
+        <input type="password" name="password" value={values.password} onChange={handleChange} required />
+        <label htmlFor="">Password</label>
+
+        <input type="submit" value="Submit form" />
+      </form>
     </div>
   )
 }
--- src/pages/SigninAndSignupPage/SigninAndSignupPage.js
+++ src/pages/SigninAndSignupPage/SigninAndSignupPage.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './SigninAndSignupPage.styles.scss'
+import Signin from '../../components/SinginAndSignup/Signin/Signin'
 
 const SigninAndSignupPage = () => {
   return (
-    <div className={}>
-      SigninAndSignupPage
+    <div>
+      <Signin />
     </div>
   )
 }
