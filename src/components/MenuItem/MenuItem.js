c712fdde52fc95b159768162536ad8650b848e80
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-07-03 14:24:18 +0300

    add  MenuItem large size className

--- src/components/MenuItem/MenuItem.js
+++ src/components/MenuItem/MenuItem.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import './MenuItem.styles.scss'
 
-const MenuItem = ({ title }) => {
+const MenuItem = ({ title, imageUrl, linkUrl, size }) => {
   return (
-    <div className="menu-item">
+    <div style={{ backgroundImage: `url(${imageUrl})` }} className={`${size} menu-item`}>
       <div className="content">
         <h1 className="title">{title}</h1>
         <span className="subtitle">SHOP NOW</span>
