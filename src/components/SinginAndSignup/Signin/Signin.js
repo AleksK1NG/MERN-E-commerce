2b67d3a45474eee71475efd4fc7193dab4303e40
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-07-04 15:28:18 +0300

    add Signin form fix

--- src/components/SinginAndSignup/Signin/Signin.js
+++ src/components/SinginAndSignup/Signin/Signin.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import './Signin.styles.scss'
 import { useForm } from '../../../hooks/useForm'
 import FormInput from '../../Shared/FormInput/FormInput'
+import './Signin.styles.scss'
 
 const Signin = () => {
   const [values, handleChange, setValues] = useForm({ email: '', password: '' })
@@ -20,12 +20,12 @@ const Signin = () => {
       <span>Sign in with your email and password</span>
 
       <form onSubmit={handleSubmit}>
-        <FormInput type="email" name="email" value={values.email} onChange={handleChange} required label="Email" />
+        <FormInput type="email" name="email" value={values.email} handleChange={handleChange} required label="Email" />
         <FormInput
           type="password"
           name="password"
           value={values.password}
-          onChange={handleChange}
+          handleChange={handleChange}
           required
           label="Password"
         />
