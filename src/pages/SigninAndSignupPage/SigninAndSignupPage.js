d07bbfb734097ef18f328dae3121aa95492142d6
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-07-05 13:48:34 +0300

    add Signup component

--- src/pages/SigninAndSignupPage/SigninAndSignupPage.js
+++ src/pages/SigninAndSignupPage/SigninAndSignupPage.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './SigninAndSignupPage.styles.scss'
 import Signin from '../../components/SinginAndSignup/Signin/Signin'
+import Signup from '../../components/SinginAndSignup/Signup/Signup'
 
 const SigninAndSignupPage = () => {
   return (
     <div>
       <Signin />
+      <Signup />
     </div>
   )
 }
