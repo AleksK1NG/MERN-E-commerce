1826acf58699d5630f121a2ae59e16a0e8862f51
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-07-06 13:21:57 +0300

    add UI Store module and toggle cart dropdown action

--- src/components/Cart/CartIcon/CartIcon.js
+++ src/components/Cart/CartIcon/CartIcon.js
@@ -1,14 +1,19 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import './CartIcon.styles.scss'
 import { ReactComponent as ShoppingIcon } from '../../../assets/shopping-bag.svg'
+import { toggleCartHidden } from '../../../storeModules/ui/uiActions'
 
-const CartIcon = () => {
+const CartIcon = ({ toggleCartHidden }) => {
   return (
-    <div className="cart-icon">
+    <div className="cart-icon" onClick={toggleCartHidden}>
       <ShoppingIcon className="shopping-icon" />
       <span className="item-count">0</span>
     </div>
   )
 }
 
-export default CartIcon
+export default connect(
+  null,
+  { toggleCartHidden }
+)(CartIcon)
---
+++ src/storeModules/ui/uiConstants.js
@@ -0,0 +1,9 @@
+/**
+ * Constants
+ * */
+import { appName } from '../../config'
+
+export const moduleName = 'ui'
+const prefix = `${appName}/${moduleName}`
+
+export const TOGGLE_CART_HIDDEN = `${prefix}/TOGGLE_CART_HIDDEN`
