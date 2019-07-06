89cb75195feabccada5bcc216821ab24cc0cbb1c
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-07-06 18:53:51 +0300

    add CartDropdown history push to checkout page handler

--- src/storeModules/ui/uiActions.js
+++ src/storeModules/ui/uiActions.js
@@ -1,12 +1,7 @@
 import { TOGGLE_CART_HIDDEN } from './uiConstants'
 
-// export const toggleCartHidden = () => ({
-//   type: TOGGLE_CART_HIDDEN
-// })
+export const toggleCartHidden = () => ({
+  type: TOGGLE_CART_HIDDEN
+})
 
-export const toggleCartHidden = () => {
-  console.log("toggle")
-  return {
-    type: TOGGLE_CART_HIDDEN
-  }
-}
+
