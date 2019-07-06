ae8bc585aaca2401df5ee300b55d2cf69147d98f
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-07-06 16:43:39 +0300

    add Cart Selectors fix for immutable.js

--- src/components/Cart/CartDropDown/CartDropDown.js
+++ src/components/Cart/CartDropDown/CartDropDown.js
@@ -1,14 +1,27 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import './CartDropDown.styles.scss'
 import CustomButton from '../../Shared/CustomButton/CustomButton'
+import {
+  cartItemsCountSelector,
+  cartItemsSelector,
+  cartItemsTotalSelector
+} from '../../../storeModules/cart/cartSelectors'
 
-const CartDropDown = () => {
+const CartDropDown = ({ cartItems, cartItemsCount, totalCost }) => {
   return (
     <div className="cart-dropdown">
-      <div className="cart-items"></div>
+      <div className="cart-items">Total cost $: {totalCost}</div>
       <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
   )
 }
 
-export default CartDropDown
+export default connect(
+  (state) => ({
+    cartItems: cartItemsSelector(state),
+    cartItemsCount: cartItemsCountSelector(state),
+    totalCost: cartItemsTotalSelector(state)
+  }),
+  {}
+)(CartDropDown)
