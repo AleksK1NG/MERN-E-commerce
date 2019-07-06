101d07016ad4db83267d29b168f257a26d288f2f
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-07-06 19:09:08 +0300

    add CheckoutItem component with cart store actions

--- src/components/CheckOut/CheckoutItem/CheckoutItem.js
+++ src/components/CheckOut/CheckoutItem/CheckoutItem.js
@@ -1,16 +1,34 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import './CheckoutItem.styles.scss'
+import { addCartItem, clearItemFromCart, deleteCartItem } from '../../../storeModules/cart/cartActions'
 
-const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => (
-  <div className="checkout-item">
-    <div className="image-container">
-      <img src={imageUrl} alt="item" />
+const CheckoutItem = ({ cartItem, deleteCartItem, addCartItem, clearItemFromCart }) => {
+  const { imageUrl, name, quantity, price } = cartItem
+  return (
+    <div className="checkout-item">
+      <div className="image-container">
+        <img src={imageUrl} alt="item" />
+      </div>
+      <span className="name">{name}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={() => deleteCartItem(cartItem)}>
+          &#10094;
+        </div>
+        <span className="value">{quantity}</span>
+        <div className="arrow" onClick={() => addCartItem(cartItem)}>
+          &#10095;
+        </div>
+      </span>
+      <span className="price">{price}</span>
+      <div className="remove-button" onClick={() => clearItemFromCart(cartItem)}>
+        &#10005;
+      </div>
     </div>
-    <span className="name">{name}</span>
-    <span className="quantity">{quantity}</span>
-    <span className="price">{price}</span>
-    <div className="remove-button">&#10005;</div>
-  </div>
-)
+  )
+}
 
-export default CheckoutItem
+export default connect(
+  null,
+  { deleteCartItem, addCartItem, clearItemFromCart }
+)(CheckoutItem)
