ad1925138f63f539e32663f4fb172092f3385f2e
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-07-06 19:00:47 +0300

    add CheckoutPage

--- src/pages/CheckOutPage/CheckOutPage.js
+++ src/pages/CheckOutPage/CheckOutPage.js
@@ -1,18 +1,41 @@
 import React from 'react'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 import { cartItemsSelector, cartItemsTotalSelector } from '../../storeModules/cart/cartSelectors'
 import './CheckOutPage.styles.scss'
+import CheckoutItem from '../../components/CheckOut/CheckoutItem/CheckoutItem'
 
-
-const CheckOutPage = ({cartItems, totalCost}) => {
+const CheckOutPage = ({ cartItems, totalCost }) => {
   return (
-    <div>
-      Check out page
+    <div className="checkout-page">
+      <div className="checkout-header">
+        <div className="header-block">
+          <span>Product</span>
+        </div>
+        <div className="header-block">
+          <span>Description</span>
+        </div>
+        <div className="header-block">
+          <span>Quantity</span>
+        </div>
+        <div className="header-block">
+          <span>Price</span>
+        </div>
+        <div className="header-block">
+          <span>Remove</span>
+        </div>
+      </div>
+      {cartItems.map((cartItem) => (
+        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+      ))}
+      <div className="total">TOTAL: ${totalCost}</div>
     </div>
   )
 }
 
-export default connect(state => ({
-  cartItems: cartItemsSelector(state),
-  totalCost: cartItemsTotalSelector(state)
-}), {})(CheckOutPage)
+export default connect(
+  (state) => ({
+    cartItems: cartItemsSelector(state),
+    totalCost: cartItemsTotalSelector(state)
+  }),
+  {}
+)(CheckOutPage)
