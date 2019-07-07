cb62376c622152530c9444ed2ce2b7e00b1907e5
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-07-07 12:29:27 +0300

    add Immer to Auth Store Module

--- src/storeModules/auth/authReducer.js
+++ src/storeModules/auth/authReducer.js
@@ -22,87 +22,78 @@ import {
   UPDATE_USER_REQUEST,
   UPDATE_USER_SUCCESS
 } from './authConstants'
+import { produce } from 'immer'
 
 /**
  * Reducer
  * */
-export const ReducerRecord = fromJS({
+export const initialState = {
   user: null,
   error: null,
   isAuthenticated: false,
   isLoading: true
-})
-
-export default function reducer(state = ReducerRecord, action) {
-  const { type, payload } = action
-
-  switch (type) {
-    case SIGN_OUT_FB_REQUEST:
-    case SIGN_UP_WITH_EMAIL_REQUEST:
-    case UPDATE_USER_REQUEST:
-    case SIGN_IN_REQUEST:
-    case SIGN_UP_REQUEST:
-    case LOAD_USER_REQUEST:
-      return state.set('isLoading', true)
-
-    case SIGN_UP_SUCCESS:
-      return state
-        .set('user', fromJS(payload.data))
-        .set('isLoading', false)
-        .set('isAuthenticated', true)
-        .set('error', null)
-
-    case SIGN_IN_SUCCESS:
-      return state
-        .set('user', fromJS(payload.data))
-        .set('error', null)
-        .set('isLoading', false)
-        .set('isAuthenticated', true)
-
-    case LOAD_USER_SUCCESS:
-      return state
-        .set('user', fromJS(payload.data))
-        .set('isLoading', false)
-        .set('isAuthenticated', true)
-        .set('error', null)
-
-    case SIGN_OUT_FB_SUCCESS:
-    case SIGN_OUT_SUCCESS:
-      return state
-        .set('isLoading', false)
-        .set('isAuthenticated', false)
-        .set('user', null)
-
-    case SIGN_OUT_FB_ERROR:
-    case SIGN_UP_WITH_EMAIL_ERROR:
-    case SIGN_IN_WITH_EMAIL_ERROR:
-    case UPDATE_USER_ERROR:
-    case SIGN_IN_ERROR:
-    case SIGN_UP_ERROR:
-    case LOAD_USER_ERROR:
-      return state.set('error', payload.error).set('isLoading', false)
-
-    case UPDATE_USER_SUCCESS:
-      return state
-        .merge({ user: fromJS(payload.data) })
-        .set('error', null)
-        .set('isLoading', false)
-
-    case SIGN_IN_WITH_EMAIL_SUCCESS:
-      return state
-        .set('user', fromJS(payload.user))
-        .set('error', null)
-        .set('isLoading', false)
-        .set('isAuthenticated', true)
-
-    case SIGN_UP_WITH_EMAIL_SUCCESS:
-      return state
-        .set('user', fromJS(payload.newUser))
-        .set('error', null)
-        .set('isLoading', false)
-        .set('isAuthenticated', true)
-
-    default:
-      return state
-  }
 }
+
+const authReducer = (state = initialState, action) =>
+  produce(state, (draft) => {
+    const { type, payload } = action
+    switch (type) {
+      case SIGN_OUT_FB_REQUEST:
+      case SIGN_UP_WITH_EMAIL_REQUEST:
+      case UPDATE_USER_REQUEST:
+      case SIGN_IN_REQUEST:
+      case SIGN_UP_REQUEST:
+      case LOAD_USER_REQUEST:
+        draft.isLoading = true
+        break
+
+      case LOAD_USER_SUCCESS:
+      case SIGN_IN_SUCCESS:
+      case SIGN_UP_SUCCESS:
+        draft.user = payload.data
+        draft.isLoading = false
+        draft.isAuthenticated = true
+        draft.error = null
+        break
+
+      case SIGN_OUT_FB_SUCCESS:
+      case SIGN_OUT_SUCCESS:
+        draft.isLoading = false
+        draft.isAuthenticated = false
+        draft.user = null
+        break
+
+      case SIGN_OUT_FB_ERROR:
+      case SIGN_UP_WITH_EMAIL_ERROR:
+      case SIGN_IN_WITH_EMAIL_ERROR:
+      case UPDATE_USER_ERROR:
+      case SIGN_IN_ERROR:
+      case SIGN_UP_ERROR:
+      case LOAD_USER_ERROR:
+        draft.error = payload.error
+        draft.isLoading = false
+        break
+
+      case UPDATE_USER_SUCCESS:
+        draft.user = { ...draft.user, ...payload.data }
+        draft.error = null
+        draft.isLoading = false
+        break
+
+      case SIGN_IN_WITH_EMAIL_SUCCESS:
+        draft.user = payload.user
+        draft.error = null
+        draft.isLoading = false
+        draft.isAuthenticated = true
+        break
+
+      case SIGN_UP_WITH_EMAIL_SUCCESS:
+        draft.user = payload.newUser
+        draft.error = null
+        draft.isLoading = false
+        draft.isAuthenticated = true
+        break
+    }
+  })
+
+export default authReducer
--- src/storeModules/auth/authSelectors.js
+++ src/storeModules/auth/authSelectors.js
@@ -9,23 +9,23 @@ export const stateSelector = (state) => state[moduleName]
 
 export const isAuthenticatedSelector = createSelector(
   stateSelector,
-  (state) => state.get('isAuthenticated')
+  (state) => state.isAuthenticated
 )
 
 export const isLoadingSelector = createSelector(
   stateSelector,
-  (state) => state.get('isLoading')
+  (state) => state.isLoading
 )
 
 export const userSelector = createSelector(
   stateSelector,
   (state) => {
-    const user = state.get('user')
-    return user ? user.toJS() : null
+    const user = state.user
+    return user
   }
 )
 
 export const authUserSelector = createSelector(
   stateSelector,
-  (state) => state.get('user')
+  (state) => state.user
 )
