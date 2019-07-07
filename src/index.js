205fffe9ccda0704069d30f7f70e51d8bf93dc47
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-07-07 13:40:22 +0300

    add redux-persist

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { ConnectedRouter } from 'connected-react-router'
 import App from './App'
-import store from './redux'
+
+import { PersistGate } from 'redux-persist/integration/react'
+import { store, persistor } from './redux'
 import history from './history'
 
 ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </ConnectedRouter>
   </Provider>,
   document.getElementById('root')
