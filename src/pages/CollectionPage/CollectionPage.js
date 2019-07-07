f451b73411ac55aa727865533ac34e6f7b852e38
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-07-07 17:46:50 +0300

    add CollectionItem width fix for use in collection page

--- src/pages/CollectionPage/CollectionPage.js
+++ src/pages/CollectionPage/CollectionPage.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import './CollectionPage.styles.scss'
-import { collectionUrlParamSelector } from '../../storeModules/shop/shopSelectors'
 
-const CollectionPage = ({ match, collection }) => {
-  debugger
+import { collectionUrlParamSelector } from '../../storeModules/shop/shopSelectors'
+import CollectionItem from '../../components/Collection/CollectionItem/CollectionItem'
+
+import './CollectionPage.styles.scss'
+
+const CollectionPage = ({ collection }) => {
+  const { title, items } = collection
+
   return (
-    <div className="collection">
-      <h2>Collection Page</h2>
+    <div className="collection-page">
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
     </div>
   )
 }
