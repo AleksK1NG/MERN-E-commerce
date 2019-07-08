9c9414bec88ede3bec6d01151ef83f8430d6b504
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-07-08 18:24:49 +0300

    add styled MenuItem

--- src/components/MenuItem/MenuItem.js
+++ src/components/MenuItem/MenuItem.js
@@ -1,16 +1,24 @@
 import React from 'react'
 import { withRouter } from 'react-router-dom'
-import './MenuItem.styles.scss'
+
+import {
+  MenuItemContainer,
+  BackgroundImageContainer,
+  ContentContainer,
+  ContentSubtitle,
+  ContentTitle
+} from './MenuItem.styles'
 
 const MenuItem = ({ title, imageUrl, linkUrl, size, history, match }) => {
   return (
-    <div className={`${size} menu-item`} onClick={() => history.push(linkUrl)}>
-      <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }} />
-      <div className="content">
-        <h1 className="title">{title.toUpperCase()}</h1>
-        <span className="subtitle">SHOP NOW</span>
-      </div>
-    </div>
+    <MenuItemContainer size={size} onClick={() => history.push(`${match.url}${linkUrl}`)}>
+      <BackgroundImageContainer className="background-image" imageUrl={imageUrl} />
+
+      <ContentContainer className="content">
+        <ContentTitle>{title.toUpperCase()}</ContentTitle>
+        <ContentSubtitle>SHOP NOW</ContentSubtitle>
+      </ContentContainer>
+    </MenuItemContainer>
   )
 }
 
