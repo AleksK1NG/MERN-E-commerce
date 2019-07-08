6b67abddd0f9d6cb802192e717f13b0f087288ee
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-07-08 16:32:34 +0300

    add styled CollectionPage

--- src/pages/CollectionPage/CollectionPage.js
+++ src/pages/CollectionPage/CollectionPage.js
@@ -2,20 +2,21 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { collectionUrlParamSelector } from '../../storeModules/shop/shopSelectors'
 import CollectionItem from '../../components/Collection/CollectionItem/CollectionItem'
-import './CollectionPage.styles.scss'
+
+import { CollectionItemsContainer, CollectionPageContainer, CollectionTitle } from './CollectionPage.styles'
 
 const CollectionPage = ({ collection }) => {
   const { title, items } = collection
 
   return (
-    <div className="collection-page">
-      <h2 className="title">{title}</h2>
-      <div className="items">
+    <CollectionPageContainer>
+      <CollectionTitle>{title}</CollectionTitle>
+      <CollectionItemsContainer>
         {items.map((item) => (
           <CollectionItem key={item.id} item={item} />
         ))}
-      </div>
-    </div>
+      </CollectionItemsContainer>
+    </CollectionPageContainer>
   )
 }
 
