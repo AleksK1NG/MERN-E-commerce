914789d9f72937921b4a26e000cfc998d7d9cfe6
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-07-11 09:47:23 +0300

    add Client Stripe payment saga

--- client/src/storeModules/shop/shopActions.js
+++ client/src/storeModules/shop/shopActions.js
@@ -1,5 +1,10 @@
-import { FETCH_COLLECTIONS_REQUEST } from './shopConstants'
+import { FETCH_COLLECTIONS_REQUEST, STRIPE_PAYMENT_REQUEST } from './shopConstants'
 
 export const fetchCollections = () => ({
   type: FETCH_COLLECTIONS_REQUEST
+})
+
+export const stripePayment = (paymentData) => ({
+  type: STRIPE_PAYMENT_REQUEST,
+  payload: { paymentData }
 })
--- client/src/storeModules/shop/shopSaga.js
+++ client/src/storeModules/shop/shopSaga.js
@@ -3,8 +3,15 @@ import { toast } from 'react-toastify'
 
 import { call, put, all, takeLatest } from 'redux-saga/effects'
 import { rejectError } from '../../utils/rejectErrorHelper'
-import { FETCH_COLLECTIONS_ERROR, FETCH_COLLECTIONS_REQUEST, FETCH_COLLECTIONS_SUCCESS } from './shopConstants'
+import {
+  FETCH_COLLECTIONS_ERROR,
+  FETCH_COLLECTIONS_REQUEST,
+  FETCH_COLLECTIONS_SUCCESS,
+  STRIPE_PAYMENT_ERROR,
+  STRIPE_PAYMENT_REQUEST
+} from './shopConstants'
 import { convertCollectionsSnapshotToMap, firestore } from '../../firebase/firebase.utils'
+import ApiService from '../../services/api'
 
 export function* fetchCollectionsSaga() {
   try {
@@ -28,6 +35,26 @@ export function* fetchCollectionsSaga() {
   }
 }
 
+export function* stripePaymentSaga({ payload }) {
+  try {
+    const data = yield call(ApiService.stripePayment, payload.paymentData)
+
+    console.log('Stripe payment data response => ', data)
+    debugger
+    toast.success('You successfully make the payment ! =D ')
+  } catch (error) {
+    console.error(error)
+    yield put({
+      type: STRIPE_PAYMENT_ERROR,
+      payload: { error }
+    })
+    toast.error('There was an issue with your payment! Please make sure you use the provided credit card.')
+  }
+}
+
 export function* saga() {
-  yield all([takeLatest(FETCH_COLLECTIONS_REQUEST, fetchCollectionsSaga)])
+  yield all([
+    takeLatest(FETCH_COLLECTIONS_REQUEST, fetchCollectionsSaga),
+    takeLatest(STRIPE_PAYMENT_REQUEST, stripePaymentSaga)
+  ])
 }
