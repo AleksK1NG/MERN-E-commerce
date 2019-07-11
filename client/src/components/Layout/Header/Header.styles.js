5f931381b57457852cfb9a8aa8cbc9269c365a1d
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-07-11 17:13:52 +0300

    add Client Header media queries for mobiles

--- client/src/components/Layout/Header/Header.styles.js
+++ client/src/components/Layout/Header/Header.styles.js
@@ -1,22 +1,29 @@
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
-// const OptionContainerStyles = css`
-//   padding: 10px 15px;
-//   cursor: pointer;
-// `
-
 export const HeaderContainer = styled.div`
   height: 70px;
   width: 100%;
   display: flex;
   justify-content: space-between;
   margin-bottom: 25px;
+
+  @media screen and (max-width: 800px) {
+    height: 60px;
+    padding: 10px;
+    margin-bottom: 20px;
+  }
 `
+
 export const LogoContainer = styled(Link)`
   height: 100%;
   width: 70px;
   padding: 25px;
+
+  @media screen and (max-width: 800px) {
+    width: 50px;
+    padding: 0;
+  }
 `
 
 export const OptionsContainer = styled.div`
@@ -25,7 +32,12 @@ export const OptionsContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-end;
+
+  @media screen and (max-width: 800px) {
+    width: 80%;
+  }
 `
+
 export const OptionLink = styled(Link)`
   padding: 10px 15px;
   cursor: pointer;
