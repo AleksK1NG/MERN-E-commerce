8c00178f171e40c14926b4cfe46650e8756b8b2e
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-07-11 11:44:14 +0300

    add Server and Client package.json settings for deploy

--- server.js
+++ server.js
@@ -14,19 +14,6 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')))
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
-  })
-}
-
-app.listen(port, (error) => {
-  if (error) throw error
-  console.log('Server running on port ' + port)
-})
-
 app.post('/payment', (req, res) => {
   const body = {
     source: req.body.token.id,
@@ -42,3 +29,16 @@ app.post('/payment', (req, res) => {
     }
   })
 })
+
+app.listen(port, (error) => {
+  if (error) throw error
+  console.log('Server running on port ' + port)
+})
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+  })
+}
