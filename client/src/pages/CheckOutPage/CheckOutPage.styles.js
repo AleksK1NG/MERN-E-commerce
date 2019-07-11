391c6046cd505cc0c03dd919e9146fe7ebc2a3a6
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-07-11 17:28:30 +0300

    add Client Mobile responsive support media queries

--- client/src/pages/CheckOutPage/CheckOutPage.styles.js
+++ client/src/pages/CheckOutPage/CheckOutPage.styles.js
@@ -7,9 +7,14 @@ export const CheckoutPageContainer = styled.div`
   flex-direction: column;
   align-items: center;
   margin: 50px auto 0;
+
   button {
     margin-left: auto;
     margin-top: 50px;
+  }
+
+  @media screen and (max-width: 800px) {
+    width: 90%;
   }
 `
 
@@ -24,8 +29,16 @@ export const CheckoutHeaderContainer = styled.div`
 export const HeaderBlockContainer = styled.div`
   text-transform: capitalize;
   width: 23%;
+
   &:last-child {
     width: 8%;
+  }
+
+  @media screen and (max-width: 800px) {
+    width: 22%;
+    &:last-child {
+      width: 12%;
+    }
   }
 `
 
