0fafae6ba5faad587a27aff32b5c7713923ff7f4
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-07-12 13:31:40 +0300

    add PWA

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { ConnectedRouter } from 'connected-react-router'
-import App from './App'
+import * as serviceWorker from './serviceWorker'
 
 import { PersistGate } from 'redux-persist/integration/react'
 import { store, persistor } from './redux'
 import history from './history'
+import './index.css'
+
+import App from './App'
 
 ReactDOM.render(
   <Provider store={store}>
@@ -18,3 +21,5 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 )
+
+serviceWorker.register()
