a97c02fa965d5fc6a1c7cc9f4ac36331dc4ba92f
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-07-16 13:10:33 +0300

    add CollectionPreview tests

--- client/src/components/Collection/CollectionPreview/CollectionPreview.js
+++ client/src/components/Collection/CollectionPreview/CollectionPreview.js
@@ -4,7 +4,7 @@ import { withRouter } from 'react-router-dom'
 
 import { CollectionPreviewContainer, PreviewContainer, TitleContainer } from './CollectionPreview.styles'
 
-const CollectionPreview = ({ title, items, history, match, routeName }) => {
+export const CollectionPreview = ({ title, items, history, match, routeName }) => {
   return (
     <CollectionPreviewContainer>
       <TitleContainer onClick={() => history.push(`${match.path}/${routeName}`)}>{title.toUpperCase()}</TitleContainer>
